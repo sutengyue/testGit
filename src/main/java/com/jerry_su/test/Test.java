613a5d1531db71d6709cd4c7ca9c4c2c79e86324
eclipse_user <136738189@qq.com>
2019-12-18 05:43:15 +0800

    second conflict test

--- src/main/java/com/jerry_su/test/Test.java
+++ src/main/java/com/jerry_su/test/Test.java
@@ -2,10 +2,8 @@ package com.jerry_su.test;
 
 public class Test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("left");
-=======
 		System.out.println("right");
->>>>>>> branch 'master' of git@github.com:sutengyue/testGit.git
+		System.out.println("ca bu taihao ");
 	}
 }
