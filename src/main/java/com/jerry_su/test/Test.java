0c663f536932f210a261ed2872cc8198050ea765
jerry_su <136738189@qq.com>
2019-12-18 05:46:54 +0800

    sss

--- src/main/java/com/jerry_su/test/Test.java
+++ src/main/java/com/jerry_su/test/Test.java
@@ -2,10 +2,7 @@ package com.jerry_su.test;
 
 public class Test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("left");
-=======
 		System.out.println("right");
->>>>>>> branch 'master' of git@github.com:sutengyue/testGit.git
 	}
 }
